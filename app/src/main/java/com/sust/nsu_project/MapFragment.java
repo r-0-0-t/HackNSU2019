55645ae70d9703588e6705de1cb5384ce9671fe4
Ahsan Aziz Ishan <ishan.bn14@gmail.com>
2019-04-04 14:44:51 +0600

    Added ToolTip

--- app/src/main/java/com/sust/nsu_project/MapFragment.java
+++ app/src/main/java/com/sust/nsu_project/MapFragment.java
@@ -1,11 +1,13 @@
 package com.sust.nsu_project;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 
 /**
@@ -19,11 +21,21 @@ public class MapFragment extends Fragment {
     }
 
 
+    ImageView im;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View v = inflater.inflate(R.layout.fragment_map, container, false);
+        im = v.findViewById(R.id.im);
+        im.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(),Profile.class));
+            }
+        });
+        return v;
     }
 
 }
