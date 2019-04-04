1d809d250cc832a7356eb0b291c549f74cbe6f57
Ahsan Aziz Ishan <ishan.bn14@gmail.com>
2019-04-04 14:36:26 +0600

    Added ToolTip

--- app/src/main/java/com/sust/nsu_project/BookProfile.java
+++ app/src/main/java/com/sust/nsu_project/BookProfile.java
@@ -1,7 +1,9 @@
 package com.sust.nsu_project;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class BookProfile extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class BookProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_profile);
+    }
+
+    public void becomesellerclick(View view) {
+        fragment = new BecomeSellerFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment,fragment).commit();
+    }
+
+    public void takeCourse(View view) {
+        fragment = new TakeCourseFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment,fragment).commit();
+    }
+    private Fragment fragment;
+    public void findSeller(View view) {
+        fragment = new MapFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment,fragment).commit();
     }
 }
