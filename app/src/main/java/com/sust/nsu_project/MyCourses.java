bd09283bd4f5703b6ea4d5d4e4a2309c61880708
Tushita(2016831026) <kazi26@student.sust.edu>
2019-04-05 09:40:27 +0600

    update

--- app/src/main/java/com/sust/nsu_project/MyCourses.java
+++ app/src/main/java/com/sust/nsu_project/MyCourses.java
@@ -1,20 +1,30 @@
 package com.sust.nsu_project;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
-public class MyCourses extends AppCompatActivity {
+public class MyCourses extends AppCompatActivity implements View.OnClickListener {
 
-    Toolbar toolbar;
+    FloatingActionButton floatbtn;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_courses);
 
-        toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("My Courses");
+        floatbtn = findViewById(R.id.floatbtn);
+        floatbtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.floatbtn){
+            Intent intent = new Intent(MyCourses.this,AddCourse.class);
+            startActivity(intent);
+        }
     }
 }
